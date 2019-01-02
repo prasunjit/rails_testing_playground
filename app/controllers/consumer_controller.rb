a3a9cc9aea55c3904e0471d8ec8faf43c4672796
Prasunjit <prasunjitnayak@gmail.com>
2019-01-02 10:54:34 -0600

    Correct navigating in the spec, add actions in the controller for the specs

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -1,6 +1,8 @@
 class ConsumerController < ApplicationController
     def consumer_home
     end
-    def consumer_card_failed
+    def consumer_data_notFound
+    end
+    def consumer_not_found
     end
 end
